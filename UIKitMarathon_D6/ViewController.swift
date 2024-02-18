35f149214f02326cd7b9a37fa0a364c82bd216a0
mostfus <mostfus@yandex-team.ru>
2024-02-18 18:12:45 +0300

    add animation

--- UIKitMarathon_D6/ViewController.swift
+++ UIKitMarathon_D6/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var animator = UIDynamicAnimator(referenceView: view)
+
+    private lazy var box: UIView = {
+        let view = UIView(frame: .zero)
+        view.frame.size = .init(width: 100, height: 100)
+        view.backgroundColor = .systemBlue
+        view.layer.cornerRadius = 12
+        view.layer.cornerCurve = .continuous
+
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(onViewTap(_:)))
+
+        box.frame.origin = .init(x: view.center.x - box.frame.size.width / 2, y: view.center.y + 200)
+
+        view.addGestureRecognizer(tapRecognizer)
+        view.addSubview(box)
     }
 
+    @objc
+    private func onViewTap(_ sender: UIGestureRecognizer) {
+        let location = sender.location(in: view)
 
+        let behavior = UISnapBehavior(
+            item: box,
+            snapTo: location
+        )
+
+        behavior.damping = 0.5
+
+        animator.removeAllBehaviors()
+        animator.addBehavior(behavior)
+    }
 }
 
